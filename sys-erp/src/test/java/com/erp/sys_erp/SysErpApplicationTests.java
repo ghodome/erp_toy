d042b41d794d7b1f55a53e6c06a1e889bd7280e2
ghodome <vjaewook@gmail.com>
2024-09-19 16:32:27 +0900

    sys-erp 생성

---
+++ sys-erp/src/test/java/com/erp/sys_erp/SysErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.sys_erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
