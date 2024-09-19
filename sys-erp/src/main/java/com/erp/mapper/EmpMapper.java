835f6e0a941728702e6d4f00721e24607d4e1027
lhubel <jkl5914@naver.com>
2024-09-19 20:13:35 +0900

    오늘은 여기까지

--- sys-erp/src/main/java/com/erp/mapper/EmpMapper.java
+++ sys-erp/src/main/java/com/erp/mapper/EmpMapper.java
@@ -1,5 +1,25 @@
 package com.erp.mapper;
 
-public class EmpMapper {
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import org.springframework.jdbc.core.RowMapper;
+
+import com.erp.dto.EmpDto;
+
+public class EmpMapper implements RowMapper<EmpDto> {
+
+	@Override
+	public EmpDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+		EmpDto empDto = new EmpDto();
+		empDto.setEmpId(rs.getString("emp_id"));
+		empDto.setEmpPassword(rs.getString("emp_password"));
+		empDto.setEmpUserType(rs.getString("emp_user_type"));
+		empDto.setEmpNo(rs.getString("emp_no"));
+		empDto.setEmpName(rs.getString("emp_name"));
+		empDto.setEmpLevel(rs.getString("emp_level"));
+		empDto.setEmpDept(rs.getString("emp_dept"));
+		return empDto;
+	}
+	
 }
