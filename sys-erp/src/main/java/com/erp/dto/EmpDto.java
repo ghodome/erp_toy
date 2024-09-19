2ad5b4590ebfdac34a595ddd964be0ebb5151555
lhubel <jkl5914@naver.com>
2024-09-19 19:40:09 +0900

    일단 저장

---
+++ sys-erp/src/main/java/com/erp/dto/EmpDto.java
@@ -0,0 +1,26 @@
+package com.erp.dto;
+
+import java.sql.Date;
+
+
+public class EmpDto {
+    private String empId;          // emp_id
+    private String empPassword;    // emp_password
+    private String empUserType;    // emp_user_type
+    private String empNo;          // emp_no
+    private String empName;        // emp_name
+    private String empLevel;       // emp_level
+    private String empDept;        // emp_dept
+    private String empGender;      // emp_gender
+    private String empHp;          // emp_hp
+    private String empEmail;       // emp_email
+    private Date empBirth;         // emp_birth
+    private Date empEdu;           // emp_edu
+    private String empSdate;       // emp_sdate
+    private String empMemo;        // emp_memo
+    private String empPost;        // emp_post
+    private String empAddress1;    // emp_address1
+    private String empAddress2;    // emp_address2
+    private String empRole;        // emp_role
+    private String empSignature;    // emp_signature
+}
