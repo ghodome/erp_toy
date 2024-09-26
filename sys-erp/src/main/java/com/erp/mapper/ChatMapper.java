91a154a565d84876fff038186f454a32bd948529
minseokOo <minseok916@gmail.com>
2024-09-26 21:27:59 +0900

    채팅 1차

---
+++ sys-erp/src/main/java/com/erp/mapper/ChatMapper.java
@@ -0,0 +1,29 @@
+package com.erp.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Service;
+
+import com.erp.dto.ChatDto;
+
+@Service
+public class ChatMapper implements RowMapper<ChatDto> {
+
+	@Override
+	public ChatDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+		ChatDto chatDto = new ChatDto();
+		
+		chatDto.setChatNo(rs.getInt("chat_no"));
+		chatDto.setChatChatroomNo(rs.getInt("chat_chatroom_no"));
+		chatDto.setChatId(rs.getString("chat_id"));
+		chatDto.setChatContent(rs.getString("chat_content"));
+		chatDto.setChatRead(rs.getInt("chat_read"));
+		chatDto.setChatTime(rs.getDate("chat_time"));
+		chatDto.setChatDelete(rs.getString("chat_delete"));
+		
+		return chatDto;
+	}
+
+}
---
+++ sys-erp/src/main/java/com/erp/restcontroller/ChatRestController.java
@@ -0,0 +1,28 @@
+package com.erp.restcontroller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.erp.dao.ChatDao;
+import com.erp.dto.ChatDto;
+
+@CrossOrigin(origins= {"http://localhost:3000"})
+@RestController
+@RequestMapping("/chat")
+public class ChatRestController {
+
+	@Autowired
+	private ChatDao chatDao;
+	
+	@GetMapping("/keyword/{keyword}")
+	public List<ChatDto> chatroomContent(@PathVariable String keyword){
+		List<ChatDto> list = chatDao.selectListByChatroomNo(keyword);
+		return list;
+	}
+}
