ac381458c6ee344c380556a8a90ae73a69262e68
lhubel <jkl5914@naver.com>
2024-09-27 01:55:21 +0900

    토큰 생성 JWTProvider 및 로그인시 JWT토큰 발행 까지 완료

--- sys-erp/src/main/java/com/erp/restcontroller/EmpRestController.java
+++ sys-erp/src/main/java/com/erp/restcontroller/EmpRestController.java
@@ -1,19 +1,19 @@
 package com.erp.restcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.erp.dao.EmpDao;
 import com.erp.dto.EmpDto;
 import com.erp.vo.LoginVO;
-
+import com.erp.dto.LoginResponseDto;
+import com.erp.util.JwtProvider;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -26,24 +26,35 @@ public class EmpRestController {
 	@Autowired
 	private PasswordEncoder encoder;
 
+	@Autowired
+	private JwtProvider jwtProvider;
+
 	// Create(등록)
 	@PostMapping("/")
 	public void insert(@RequestBody EmpDto empDto) {
-
 		empDao.insert(empDto);
-
 	}
 
 	// 로그인
-	// home.jsp에서 내용 받아오기
 	@PostMapping("/login")
-	public boolean login(@RequestBody LoginVO loginVO) {
+	public ResponseEntity<LoginResponseDto> login(@RequestBody LoginVO loginVO) {
 		EmpDto empDto = empDao.selectOneById(loginVO.getEmpId());
 		if (empDto != null && encoder.matches(loginVO.getEmpPassword(), empDto.getEmpPassword())) {
-			return true;
+			String accessToken = jwtProvider.generateToken(empDto.getEmpId(), empDto.getEmpEmail());
+			String refreshToken = jwtProvider.generateRefreshToken(empDto.getEmpId(), empDto.getEmpEmail());
+
+			// 토큰 내용을 콘솔에 출력
+			System.out.println("Access Token: " + accessToken);
+			System.out.println("Refresh Token: " + refreshToken);
+
+			// LoginResponseDto 객체 생성
+			LoginResponseDto responseDto = new LoginResponseDto();
+			responseDto.setAccessToken(accessToken);
+			responseDto.setRefreshToken(refreshToken);
+
+			return ResponseEntity.ok(responseDto);
 		} else {
-			return false;
+			return ResponseEntity.status(401).body(null); // Unauthorized
 		}
 	}
-
 }
