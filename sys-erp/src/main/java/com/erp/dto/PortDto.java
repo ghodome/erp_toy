41d02f41826b481e3528acdc08ddc8adfe7040e9
ghodome <vjaewook@gmail.com>
2024-09-27 21:30:56 +0900

    입출고 기본 crud 구현중

---
+++ sys-erp/src/main/java/com/erp/dto/PortDto.java
@@ -0,0 +1,15 @@
+package com.erp.dto;
+
+import lombok.Data;
+
+@Data
+public class PortDto {
+	private int portNo;
+	private int portProductNo;
+	private String portWorker;
+	private int portStorageNo;
+	private int portQty;
+	private int portTradingValue;
+	private int portCorrespondent;
+	private int portUnit;
+}
