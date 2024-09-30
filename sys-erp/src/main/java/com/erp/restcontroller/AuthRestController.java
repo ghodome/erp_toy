8b5fa3c3a4196f44935a11d00e9a439200800093
IhubeI <jkl5914@naver.com>
2024-09-30 21:15:43 +0900

    수정완료

--- sys-erp/src/main/java/com/erp/restcontroller/AuthRestController.java
+++ sys-erp/src/main/java/com/erp/restcontroller/AuthRestController.java
@@ -1,11 +1,13 @@
 package com.erp.restcontroller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 // 인증 관련 RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/auth")
 public class AuthRestController {
