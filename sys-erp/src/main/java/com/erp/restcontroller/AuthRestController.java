5ee29025a6b0943b5c02ec210c8c360152a8b8b3
IhubeI <jkl5914@naver.com>
2024-10-02 10:09:32 +0900

    수정3

--- sys-erp/src/main/java/com/erp/restcontroller/AuthRestController.java
+++ sys-erp/src/main/java/com/erp/restcontroller/AuthRestController.java
@@ -24,7 +24,9 @@ public class AuthRestController {
 
 	@PostMapping("/")
     public void auth() {
+		
         // 인증이 완료된 상태에서 요청이 들어오면,
         // 사용자의 정보를 반환하거나, 필요한 추가 작업을 수행할 수 있습니다.
+		
     }
 }
