e3419dc5d08f83357cb237d9e52779a5fe7d9167
IhubeI <jkl5914@naver.com>
2024-10-08 17:55:53 +0900

    JWT 인증 경로 변경

--- sys-erp/src/main/java/com/erp/util/JwtAuthenticationFilter.java
+++ sys-erp/src/main/java/com/erp/util/JwtAuthenticationFilter.java
@@ -23,110 +23,117 @@ import jakarta.servlet.http.HttpServletResponse;
 @Service
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    @Autowired
-    private SqlSession sqlSession;
+	@Autowired
+	private SqlSession sqlSession;
 
-    @Autowired
-    private JwtProvider jwtProvider;
+	@Autowired
+	private JwtProvider jwtProvider;
 
-    @Autowired
-    private CustomUserDetailsService customUserDetailsService;
+	@Autowired
+	private CustomUserDetailsService customUserDetailsService;
 
-    @Override
-    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-        String[] excludePath = { "/swagger-ui/**", "/v3/**", "/swagger-resources/**", "/webjars/**", "/api-docs/**", "/emp/login" };
-        String path = request.getRequestURI();
+	@Override
+	protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+//        String[] excludePath = { "/emp/", "/swagger-ui/**", "/v3/**", "/swagger-resources/**", "/webjars/**", "/api-docs/**", "/emp/login" };
+//        String path = request.getRequestURI();
+//
+//        // 경로가 매칭되는지 확인
+//        return Arrays.stream(excludePath).anyMatch(pattern -> path.matches(pattern.replace("**", ".*")));
 
-        // 경로가 매칭되는지 확인
-        return Arrays.stream(excludePath).anyMatch(pattern -> path.matches(pattern.replace("**", ".*")));
-    }
+		// 필터가 적용될 경로
+		String[] includePath = { "/emp/me" };
+		String path = request.getRequestURI();
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
+		// 경로가 매칭되는지 확인
+		return Arrays.stream(includePath).noneMatch(pattern -> path.matches(pattern.replace("**", ".*")));
+	}
 
-        System.out.println("====================In Filter======================");
-        System.out.println("Filtering request for URI: " + request.getRequestURI());
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
 
-        Cookie[] cookies = request.getCookies();
-        String accessToken = null;
-        String refreshToken = null;
-        String username = null;
+		System.out.println("====================In Filter======================");
+		System.out.println("Filtering request for URI: " + request.getRequestURI());
 
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("accessToken")) {
-                    accessToken = cookie.getValue();
-                    System.out.println("AccessToken: " + accessToken);
-                } else if (cookie.getName().equals("refreshToken")) {
-                    refreshToken = cookie.getValue();
-                    System.out.println("리프레시 토큰 : " + refreshToken);
-                }
-            }
-        }
+		Cookie[] cookies = request.getCookies();
+		String accessToken = null;
+		String refreshToken = null;
+		String username = null;
 
-        // 토큰이 없을 때 401 오류 반환
-        if (accessToken == null && refreshToken == null) {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authorization token is missing");
-            return;
-        }
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals("accessToken")) {
+					accessToken = cookie.getValue();
+					System.out.println("AccessToken: " + accessToken);
+				} else if (cookie.getName().equals("refreshToken")) {
+					refreshToken = cookie.getValue();
+					System.out.println("리프레시 토큰 : " + refreshToken);
+				}
+			}
+		}
 
-        try {
-            if (accessToken != null && jwtProvider.validateToken(accessToken)) {
-                // 유효한 액세스 토큰인 경우, 사용자 인증 처리
-                username = jwtProvider.extractAllClaims(accessToken).get("loginId", String.class);
-                System.out.println("상태 : 유효한 AccessToken 존재!");
+		// 토큰이 없을 때 401 오류 반환
+		if (accessToken == null && refreshToken == null) {
+			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authorization token is missing");
+			return;
+		}
 
-            } else if (refreshToken != null && jwtProvider.validateToken(refreshToken)) {
-                // 액세스 토큰이 없고 리프레시 토큰이 유효한 경우
-                username = jwtProvider.extractAllClaims(refreshToken).get("loginId", String.class);
-                EmpDto empDto = sqlSession.selectOne("emp.selectEmpById", username);
+		try {
+			if (accessToken != null && jwtProvider.validateToken(accessToken)) {
+				// 유효한 액세스 토큰인 경우, 사용자 인증 처리
+				username = jwtProvider.extractAllClaims(accessToken).get("loginId", String.class);
+				System.out.println("상태 : 유효한 AccessToken 존재!");
 
-                if (empDto == null) {
-                    throw new RuntimeException("사용자를 찾을 수 없습니다."); // 사용자 정보가 없을 때 예외 발생
-                }
+			} else if (refreshToken != null && jwtProvider.validateToken(refreshToken)) {
+				// 액세스 토큰이 없고 리프레시 토큰이 유효한 경우
+				username = jwtProvider.extractAllClaims(refreshToken).get("loginId", String.class);
+				EmpDto empDto = sqlSession.selectOne("emp.selectEmpById", username);
 
-                // 새로운 액세스 토큰 생성
-                String newAccessToken = jwtProvider.generateToken(empDto.getEmpId(), empDto.getEmpEmail(),
-                        empDto.getEmpRole());
+				if (empDto == null) {
+					throw new RuntimeException("사용자를 찾을 수 없습니다."); // 사용자 정보가 없을 때 예외 발생
+				}
 
-                // 새로운 액세스 토큰을 쿠키에 설정
-                Cookie newAccessTokencookie = new Cookie("accessToken", newAccessToken);
-                newAccessTokencookie.setPath("/");
-                newAccessTokencookie.setHttpOnly(true);
-                newAccessTokencookie.setSecure(false);
-                newAccessTokencookie.setMaxAge(60 * 5); // 5분
+				// 새로운 액세스 토큰 생성
+				String newAccessToken = jwtProvider.generateToken(empDto.getEmpId(), empDto.getEmpEmail(),
+						empDto.getEmpRole());
 
-                response.addCookie(newAccessTokencookie);
-                System.out.println("새로운 AccessToken 발급!");
+				// 새로운 액세스 토큰을 쿠키에 설정
+				Cookie newAccessTokencookie = new Cookie("accessToken", newAccessToken);
+				newAccessTokencookie.setPath("/");
+				newAccessTokencookie.setHttpOnly(true);
+				newAccessTokencookie.setSecure(false);
+				newAccessTokencookie.setMaxAge(60 * 5); // 5분
 
-                // 사용자를 인증 처리
-                username = jwtProvider.extractAllClaims(newAccessToken).get("loginId", String.class);
-            }
+				response.addCookie(newAccessTokencookie);
+				System.out.println("새로운 AccessToken 발급!");
 
-            // 사용자 인증 처리
-            if (username != null) {
-                UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+				// 사용자를 인증 처리
+				username = jwtProvider.extractAllClaims(newAccessToken).get("loginId", String.class);
+			}
 
-                if (userDetails == null) {
-                    throw new RuntimeException("사용자 인증 실패."); // 인증 실패 시 예외 발생
-                }
+			// 사용자 인증 처리
+			if (username != null) {
+				UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
 
-                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities());
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            } else {
-                // 토큰이 유효하지 않거나 사용자 정보를 찾을 수 없는 경우 401 반환
-                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid or missing token");
-                return;
-            }
+				if (userDetails == null) {
+					throw new RuntimeException("사용자 인증 실패."); // 인증 실패 시 예외 발생
+				}
 
-        } catch (Exception e) {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Access Denied"); // 401 오류 응답
-            return; // 필터 체인 종료
-        }
+				UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+						userDetails, null, userDetails.getAuthorities());
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+			} else {
+				// 토큰이 유효하지 않거나 사용자 정보를 찾을 수 없는 경우 401 반환
+				response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid or missing token");
+				return;
+			}
 
-        filterChain.doFilter(request, response);
-        System.out.println("===================================================");
-    }
+		} catch (Exception e) {
+			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Access Denied"); // 401 오류 응답
+			return; // 필터 체인 종료
+		}
+
+		filterChain.doFilter(request, response);
+		System.out.println("===================================================");
+	}
 }
